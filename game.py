7681022637fd2ccfe019b3e9e27a8f63bf5c9287
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-06-30 23:42:10 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,116 +1,92 @@
-#Stone, Paper and Scissor Game
-#This game made by sahil
-
+from tkinter import *
 import random
-import time
+from tkinter import messagebox
 
-player = input("Enter your Name:: ")
-time.sleep(1)
+win = Tk()
+win.title('Rock Paper Scissors')
+win.geometry('500x365')
+win.config(bg = 'black')
+win.resizable(0,0)
 
-def main( ):
-    com = input("Are you ready to play(y/n): ")
-    if com.lower( ) == "y":
-        time.sleep(1)
-        print("Good Luck",player,"...")
-    elif com.lower( ) == "n":
-        time.sleep(1)
-        print("OK...")
-        exit( )
+def info():
+    messagebox.showinfo('Info','rock<paper<scissor\nCreator: Sahil Rajwar')
+
+def rock():
+    obj = ['rock','paper','scissor']
+    view = random.choice(obj)
+    your_label.config(text = 'rock')
+    computer_label.config(text = view)
+    if view == 'rock':
+        outcomes_label.config(text = 'Tie', fg = 'grey')
+    elif view == 'paper':
+        outcomes_label.config(text = 'You Lose', fg = 'red')
+    elif view == 'scissor':
+        outcomes_label.config(text = 'You Win', fg = 'green')
     else:
-        print("You Have to give answer in (y/n) please. Try Again... :( ")
-        main( )
-main( )
+        outcomes_label.config(text = 'Error', fg = 'purple')
 
-time.sleep(2)
-print("Loading...")
-time.sleep(2)
-
-def main( ):
-
-    list = ["rock","paper","scissor"]
-    view = random.choice(list)
-
-    command = input("Choose your object(rock/paper/scissor) or type 'e' to exit!:: ")
-    if command.lower( ) == "rock":
-        print(view)
-        if view == "rock":
-            time.sleep(1)
-            print("Nice Try",player,"Match Tie!")
-            time.sleep(1)
-            print("rock = rock")
-        elif view == "scissor":
-            time.sleep(1)
-            print("Nice Match",player,"...You Win!")
-            time.sleep(1)
-            print("rock > scissor")
-        elif view == "paper":
-            time.sleep(1)
-            print("Better Luck Next",player,"You Lose")
-            time.sleep(1)
-            print("rock < paper")
-        else:
-            time.sleep(1)
-            print("ERROR :(")
-        main( )
-    elif command.lower( ) == "paper":
-        print(view)
-        if view == "rock":
-            time.sleep(1)
-            print("Nice Match",player,"...You Win!")
-            time.sleep(1)
-            print("paper > rock")
-        elif view == "scissor":
-            time.sleep(1)
-            print("Better Luck next time",player,"You Loose!")
-            time.sleep(1)
-            print("paper < scissor")
-        elif view == "paper":
-            time.sleep(1)
-            print("Nice Try",player,"... Match Tie!")
-            time.sleep(1)
-            print("paper = paper")
-        else:
-            time.sleep(1)
-            print("ERROR :(")
-        main( )
-
-    elif command.lower( ) == "scissor":
-        print(view)
-        if view == "rock":
-            time.sleep(1)
-            print("Better luck next time",player,"You Loose!")
-            time.sleep(1)
-            print("scissor < rock")
-        elif view == "scissor":
-            time.sleep(1)
-            print("Nice try",player,"... Match Tie!")
-            time.sleep(1)
-            print("scissor = scissor")
-        elif view == "paper":
-            time.sleep(1)
-            print("Nice Match",player,"...You Win!")
-            time.sleep(1)
-            print("scissor > paper")
-        else:
-            time.sleep(1)
-            print("ERROR :(")
-        main( )
-    elif command.lower( ) == "e":
-        ask = input("are you sure(y/n)")
-        if ask.lower( ) == "y":
-            time.sleep(1)
-            print("Hope You Enjoy This game :)")
-            time.sleep(1)
-            exit( )
-        elif ask.lower( ) == "n":
-            time.sleep(1)
-            print("OK...")
-            main( )
-        else:
-            time.sleep(1)
-            print("Please answer in (y/n) try again")
-            main( )
+def paper():
+    obj = ['rock','paper','scissor']
+    view = random.choice(obj)
+    your_label.config(text = 'paper')
+    computer_label.config(text = view)
+    if view == 'rock':
+        outcomes_label.config(text = 'You Win', fg = 'green')
+    elif view == 'paper':
+        outcomes_label.config(text = 'Tie', fg = 'grey')
+    elif view == 'scissor':
+        outcomes_label.config(text = 'You Lose', fg = 'red')
     else:
-        print("!!Choose the correct object!!")
-        main( )
-main( )
+        outcomes_label.config(text = 'Error', fg = 'purple')
+
+def scissor():
+    obj = ['rock','paper','scissor']
+    view = random.choice(obj)
+    your_label.config(text = 'scissor')
+    computer_label.config(text = view)
+    if view == 'rock':
+        outcomes_label.config(text = 'You Lose', fg = 'red')
+    elif view == 'paper':
+        outcomes_label.config(text = 'You Win', fg = 'green')
+    elif view == 'scissor':
+        outcomes_label.config(text = 'Tie', fg = 'grey')
+    else:
+        outcomes_label.config(text = 'Error', fg = 'purple')
+
+heading_frame = Frame(win, bd = 6, relief = SUNKEN)
+heading_frame.pack(fill = BOTH, padx = 10, pady = 10)
+
+heading_label = Label(heading_frame, font = ('Comic sans ms',20,'bold','italic'), text = 'Rock Paper Scissor')
+heading_label.pack(fill = BOTH, padx = 10, pady = 10)
+
+main_frame = Frame(win, bd = 6, relief = SUNKEN)
+main_frame.pack(fill = BOTH, padx = 10, pady = 10)
+
+you_label = Label(main_frame, font = ('Comic sans ms',12,'bold','italic'), text = ':Your Decision:',fg = 'green')
+you_label.grid(row = 0, column = 0, padx = 10, pady = 10)
+your_label = Label(main_frame, font = ('Comic sans ms',12,'bold','italic'))
+your_label.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+comp_label = Label(main_frame, font = ('Comic sans ms',12,'bold','italic'), text = ':Computer Decicsion:',fg = 'red')
+comp_label.grid(row = 1, column = 0, padx = 10, pady = 10)
+computer_label = Label(main_frame, font = ('Comic sans ms',12,'bold','italic'))
+computer_label.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+result_label = Label(main_frame, font = ('Comic sans ms',12,'bold','italic'), text = ':Results:',fg = 'black')
+result_label.grid(row = 2, column = 0, padx = 10, pady = 10)
+outcomes_label = Label(main_frame, font = ('Comic sans ms',12,'bold','italic'))
+outcomes_label.grid(row = 2, column = 1, padx = 10, pady = 10)
+
+btn_frame = Frame(win, bd = 6, relief = SUNKEN)
+btn_frame.pack(fill = BOTH, padx = 10, pady = 10)
+rock_btn = Button(btn_frame, font = ('Comic sans ms',12), text = 'Rock', command = rock)
+rock_btn.grid(row = 0, column = 0, padx = 10, pady = 10, ipadx = 10)
+paper_btn = Button(btn_frame, font = ('Comic sans ms',12), text = 'Paper', command = paper)
+paper_btn.grid(row = 0, column = 1, padx = 10, pady = 10, ipadx = 10)
+scissor_btn = Button(btn_frame, font = ('Comic sans ms',12), text = 'Scissor', command = scissor)
+scissor_btn.grid(row = 0, column = 2, padx = 10, pady = 10, ipadx = 10)
+info_btn = Button(btn_frame, font = ('Comic sans ms',12), text = 'Info', command = info)
+info_btn.grid(row = 0, column = 3, padx = 10, pady = 10, ipadx = 10)
+
+
+mainloop()
